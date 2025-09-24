2852c4dfd3b3f96882709feacb0c9f3e63a4b248
AffirmerAu <155203791+AffirmerAu@users.noreply.github.com>
2025-09-24 15:54:11 +1000

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
-// drizzle.config.ts (at repo root)
-import 'dotenv/config'
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL, ensure the database is provisioned");
+}
+
 export default defineConfig({
-  schema: './server/shared/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  out: "./migrations",
+  schema: "./shared/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEON_DATABASE_URL || process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
-})
+});
